7657682c36304c1eb4b843de67475b0ca4922a4c
Kamalesh Kumar <lovelykamal014@gmail.com>
2026-07-22 00:50:28 +0530

    Implement alert on continue button click
    Add event listener to continue button for alert.

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+document.getElementById("continueBtn").addEventListener("click", function () {
+    alert("Welcome to the next surprise! ❤️");
 
+    // அடுத்த page-க்கு பிறகு மாற்றுவோம்
+    // window.location.href = "balloons.html";
+});
